35138d855b901bdae8bf0003ad2cb896c4f428ac
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2021-05-24 10:23:35 +1200

    Added Code Comments

--- main.py
+++ main.py
@@ -1,64 +1,113 @@
 import random
 
 
+# This function will repeatedly ask the
+# user a question for a yes or no
+#
+# If the user says yes the function will return True
+# If the user says no the function will return False
 def ask_yes_no_question(question: str) -> bool:
+    # Forever
     while True:
+        # Ask the user for input
+        # and strip the whitespace
+        # and make all the characters lowercase
         answer: str = input(question).strip().lower()
 
+        # If the user says "yes" or "y"
         if answer == "yes" or answer == "y":
+            # Return out of the function and return True
             return True
+        # If the user says "no" or "n"
         elif answer == "no" or answer == "n":
+            # Return out of the function and return False
             return False
+        # If the user answers with anything else
         else:
+            # Continue the loop asking the user for an input
             print("Please enter yes or no")
 
 
+# Displays the instructions
 def show_instructions() -> None:
     print("No instructions are made right now")
 
 
+def ask_user_rock_paper_scissors() -> chr:
+    # Forever
+    while True:
+        # Ask the user for input and strip it of whitespace
+        # and put it into lowercase
+        answer: str = input("Enter rock (r), paper (p), or scissors (s), or (q) to quit ").strip().lower()
+
+        # If the user inputs "rock" or "r"
+        if answer == "rock" or answer == "r":
+            # Return out of the function and return 'r'
+            return 'r'
+        # If the user inputs "paper" or "p"
+        elif answer == "paper" or answer == "p":
+            # Return out of the function and return 'p'
+            return 'p'
+        # If the user inputs "scissors" or "s"
+        elif answer == "scissors" or answer == "s":
+            # Return out of the function and return 's'
+            return 's'
+        # If the user inputs "gun"
+        elif answer == "gun":
+            # Print some messages
+            print("You found the secret weapon!")
+            print("[Computer]: You cheated :(")
+            print("[Computer]: Goodbye")
+            # Return the 0 character as a message to quit
+            return '\0'
+        # If the user inputs "quit" or "q"
+        elif answer == "quit" or answer == "q":
+            # Return the 0 character as a message to quit
+            return '\0'
+        # If the user answers with anything else
+        else:
+            # Continue the loop asking for an answer
+            print("Please enter a valid answer\n")
+
+
 def main() -> None:
+    # Ask the user if they know how to play
     if not ask_yes_no_question("Do you know how to play? "):
+        # Show the instructions
         show_instructions()
 
+    # Newline for separation
     print()
 
+    # Forever
     while True:
-        player_answer: chr = '\0'
+        # Ask the player for input
+        player_answer: chr = ask_user_rock_paper_scissors()
 
-        while True:
-            answer: str = input("Enter rock (r), paper (p), or scissors (s), or (q) to quit ").strip().lower()
+        # If the answer is the quit character
+        if player_answer == '\0':
+            # Exit the main function
+            return
 
-            if answer == "rock" or answer == "r":
-                player_answer = 'r'
-                break
-            elif answer == "paper" or answer == "p":
-                player_answer = 'p'
-                break
-            elif answer == "scissors" or answer == "s":
-                player_answer = 's'
-                break
-            elif answer == "gun":
-                print("You found the secret weapon!")
-                print("[Computer]: You cheated :(")
-                print("[Computer]: Goodbye")
-                return
-            elif answer == "quit" or answer == "q":
-                return
-            else:
-                print("Please enter a valid answer\n")
-
+        # Possible chances for the computer
         possible_answers: list[chr] = ['r', 'p', 's']
+        # Make the computer chose a random answer
+        computer_answer: chr = random.choice(possible_answers)
+
+        # Maps the characters to strings
         answer_to_string: dict[chr, str] = {
             'r': "rock",
             'p': "paper",
             's': "scissors",
         }
 
-        computer_answer: chr = random.choice(possible_answers)
+        # Print what the player chose using the character to string map
         print(f"You chose {answer_to_string[player_answer]}")
+
+        # Print what the computer chose using the character to string map
         print(f"The computer chose {answer_to_string[computer_answer]}")
 
+        # Check for win or loose
         if player_answer == 'r':
             if computer_answer == 'r':
                 print("Tie!")
@@ -81,8 +130,11 @@ def main() -> None:
             elif computer_answer == 's':
                 print("Tie!")
 
+        # Extra newline for separation
         print()
 
 
+# If this file is not included as a module
 if __name__ == "__main__":
+    # Run the main function
     main()
